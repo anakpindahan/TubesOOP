3d58a89f6680255ee7b7a5281eb33956e09827ce
AwwalaK <awwalakamila01@gmail.com>
2021-03-25 18:00:25 +0700

    update Battle

---
+++ Battle.hpp
@@ -0,0 +1,27 @@
+#ifndef BATTLE_H
+#define BATTLE_H
+
+#include <iostream>
+#include "Skill.hpp"
+#include "Engimon.hpp"
+#include "WildEngimon.h"
+
+using namespace std;
+
+class Battle : public WildEngimon{
+    private:
+        int totalPower;
+        int elementAdvPlayer;
+        int elementAdvEnemy;
+
+    public:
+    	Battle();
+    	int sum(int);
+		int TotalPower(WildEngimon, int, Skill);
+		void showTotalPower();
+		void elementAdv1(Engimon, Engimon);
+		void tanding(WildEngimon, WildEngimon, Skill, Skill);
+        
+};
+
+#endif
